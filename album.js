8e2db6c8bc738d37ee0deed45ccf15c522d2e703
Freja Jørgensen <freja@Macbook-Freja.local>
2019-09-17 22:12:09 +0200

    CSS rettelser og kommentarer til JS

--- album.js
+++ album.js
@@ -1,18 +1,15 @@
-const urlParams = new URLSearchParams(window.location.search);
-
+const urlParams = new URLSearchParams(window.location.search); // Parameter med fra forrige side, indeholder album id
 
 /*album kommer fra det andet script dokument script.js, der hvor vi linker over til den nye side */
-const id = urlParams.get("album");
+const id = urlParams.get("album"); // Værdien af album hives ud af parameter og gemmes i id
 console.log(id);
 
-let album;
+let album = []; // Tomt array der skal indeholde albummets data
 
-const url = "https://spreadsheets.google.com/feeds/list/1JZaKWrVtkj-qGVCfIds-V04jho2DVpx5GrNsChLv_Sk/od6/public/values?alt=json"
+document.addEventListener("DOMContentLoaded", getJson); // Program går igang når DOM er loaded
 
-document.addEventListener("DOMContentLoaded", getJson);
-
-async function getJson() {
-    const jsonData = await fetch(url);
+async function getJson() { //json filerne hentes og albums lægges i album det tomme array og funktionen viewAlbum kaldes
+    const jsonData = await fetch("https://spreadsheets.google.com/feeds/list/1JZaKWrVtkj-qGVCfIds-V04jho2DVpx5GrNsChLv_Sk/od6/public/values?alt=json");
     console.log("jsonData", jsonData);
     album = await jsonData.json();
     viewAlbum();
@@ -20,7 +17,7 @@ async function getJson() {
 
 function viewAlbum() {
     console.log("view album", album);
-    album.feed.entry.forEach((cd) => {
+    album.feed.entry.forEach((cd) => { //Album-array gennemgås cd for cd og en section udfyldes for det album der passer til id'et
 
         console.log(cd.gsx$id.$t);
         if (cd.gsx$id.$t == id) {
@@ -32,10 +29,11 @@ function viewAlbum() {
             document.querySelector("#view .udgivelsesår").textContent = ` Udgivelsesår: ${cd.gsx$udgivelsesår.$t}`;
             document.querySelector("#view .genre").textContent = `Genre: ${ cd.gsx$genre.$t}`;
             document.querySelector(".view_boks").classList.add(cd.gsx$genre.$t);
-            var sange = cd.gsx$track.$t.split(",");
-            sange.forEach((sang) => {
-                var li = document.createElement("li");
-                var text = document.createTextNode(sang);
+
+            let sange = cd.gsx$track.$t.split(","); // Vi deler tracklisten ved kommaer og gemmer dem som et array i variablen sange
+            sange.forEach((sang) => { //gemmenløber arrayet sange og lavet li element for hver sang og tilføjer dem til .track
+                let li = document.createElement("li");
+                let text = document.createTextNode(sang);
                 li.appendChild(text);
                 document.querySelector("#view .track").appendChild(li);
             });
@@ -43,5 +41,5 @@ function viewAlbum() {
     })
 }
 document.querySelector(".close").addEventListener("click", () => {
-    history.back();
+    history.back(); // Ved klik på tilbage knap går vi tilbage til forrige side
 });
--- script.js
+++ script.js
@@ -1,15 +1,20 @@
-let alleGenre = [];
-let filterKategori = "alle";
-const temp = document.querySelector("template");
-const modtagerKloner = document.querySelector(".modtager");
+let alleGenre = []; // tomt array - her skal alle albums fra json fil lægges
+let filterKategori = "alle"; // alle skal vises som udgangspunkt
+const temp = document.querySelector("template"); // Er vores struktur som vi bruger til at klone albums ind i
+const modtagerKloner = document.querySelector(".modtager"); // tom liste der skal indeholde album
+
+// Konstanter der definere vores gradients for hver genre
 const rockGradient = "linear-gradient(to top, rgb(185, 45, 83) 30%, rgba(255, 255, 255, 0) 50%)";
 const danskGradient = "linear-gradient(to top, rgb(149, 217, 89) 30%, rgba(255, 255, 255, 0) 50%)";
 const popGradient = "linear-gradient(to top, rgb(249, 236, 77) 30%, rgba(255, 255, 255, 0) 50%)";
 const countryGradient = "linear-gradient(to top, rgb(134, 228, 228) 30%, rgba(255, 255, 255, 0) 50%)";
 const julGradient = "linear-gradient(to top, rgb(129, 73, 255) 30%, rgba(255, 255, 255, 0) 50%)";
-document.addEventListener("DOMContentLoaded", getJson);
 
-async function getJson() {
+document.addEventListener("DOMContentLoaded", getJson); // Program går igang når DOM er loaded
+
+
+async function getJson() { //json filerne hentes og albums lægges i alleGenre det tomme array og funktionerne visGenre og addEventListenerToButtons kaldes
+
     let jsonData = await fetch("https://spreadsheets.google.com/feeds/list/1JZaKWrVtkj-qGVCfIds-V04jho2DVpx5GrNsChLv_Sk/od6/public/values?alt=json");
     console.log("jsonData", jsonData);
     alleGenre = await jsonData.json();
@@ -18,15 +23,17 @@ async function getJson() {
 }
 
 function visGenre() {
-    modtagerKloner.innerHTML = "";
+    modtagerKloner.innerHTML = ""; // ModtagerKloner nulstiller/tømmes
 
     console.log(alleGenre);
 
-    alleGenre.feed.entry.forEach(genre => {
+    alleGenre.feed.entry.forEach(genre => { //Genre-array gennemgås genre for genre og en article udfyldes for hvert album og tilføjes til modtagerKloner hvis de har den rette filter kategori
         if ((filterKategori == "alle" || genre.gsx$genre.$t == filterKategori)) {
             const klon = temp.cloneNode(true).content;
             klon.querySelector("h2").textContent = genre.gsx$titel.$t
             klon.querySelector(".artist").textContent = genre.gsx$artist.$t;
+
+            // afhængig af albummets genre skal der tilføjes en specifik gradient defineret i konstanterne i starten. Disse bliver sat som baggrundsbillede sammen med albummets billede. Gradienten er først, da det første "billede" lægger øverst når man bruger flere billeder som baggrund, og en gradient fungere som et billede
             if (genre.gsx$genre.$t == "rock") {
                 klon.querySelector("article").style.backgroundImage = `${rockGradient}, url(img/${genre.gsx$billede.$t})`;
             } else if (genre.gsx$genre.$t == "dansk") {
@@ -40,6 +47,8 @@ function visGenre() {
             }
             klon.querySelector("article").classList.add(genre.gsx$genre.$t);
             modtagerKloner.appendChild(klon);
+
+            // Vælger det sidst tilføjede element i modtagerKloner og tilføjer en eventlistner for "click" som går til album.html med albummets id som parameter
             modtagerKloner.lastElementChild.addEventListener("click", () => {
                 location.href = `album.html?album=${genre.gsx$id.$t}`;
             });
@@ -47,13 +56,13 @@ function visGenre() {
     });
 }
 
-function addEventListenerToButtons() {
+function addEventListenerToButtons() { // lytter efter "click" på filterknapperne kalder funktionen filtrering
     document.querySelectorAll(".filter").forEach(elm => {
         elm.addEventListener("click", filtrering);
     });
 }
 
-function filtrering() {
+function filtrering() { // Finder den kategori knap der er klikket på og skifter class for den valgte knap inden vi kalder visGenre funktionen
     filterKategori = this.dataset.kategori;
     document.querySelectorAll(".filter").forEach(elm => {
         elm.classList.remove("valgt");
